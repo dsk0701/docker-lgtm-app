e60656def099f9fa32cda7adfb2234740b49b722
dsk0701 <d.dubandtribal@gmail.com>
2017-10-06 10:14:46 +0900

    Add environment variable.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+export DEV_DATABASE_USERNAME=${DB_USER:-root}
+export DEV_DATABASE_PASSWORD=${DB_PASSWORD}
+export DEV_DATABASE_NAME=${DB_NAME:-lgtm}
+
 bundle exec rake db:create
 bundle exec rake db:migrate
-
